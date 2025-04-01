4216ec3026724281642ef96b785e6ba4bcd099e1
PORNETWORK-PC\PorNe <poracademic2020@gmail.com>
2025-04-01 16:30:23 +0700

    fix(home): remove spacing

--- pages/home/index.tsx
+++ pages/home/index.tsx
@@ -2,17 +2,17 @@ import React from 'react'
 import HomeScreen from '@/features/home/screen'
 
 interface Props {
-    
+
 }
 
 const HomePage: React.FC<Props> = (props) => {
-const {} = props
+	const { } = props
 
-    return (
-        <div>
-            <HomeScreen/>
-        </div>
-    )
+	return (
+		<div>
+			<HomeScreen />
+		</div>
+	)
 }
 
 export default React.memo<Props>(HomePage)
